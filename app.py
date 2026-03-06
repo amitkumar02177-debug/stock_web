0b0c49711fe21dee3982adbcbbe97e5b02f7dbf8
amitkumar02177-debug <amitkumar02177@gmail.com>
2026-03-07 01:07:54 +0530

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
 import json
 import os
 
 app = Flask(__name__)
 
-DATA_FILE = "stocks.json"
-
+DATA_FILE = "stock.json"
 
 @app.route("/")
 def home():
 
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "r") as f:
-            products = json.load(f)
-    else:
+    if not os.path.exists(DATA_FILE):
         products = []
+    else:
+        with open(DATA_FILE, "r") as f:
+            content = f.read().strip()
+
+            if content == "":
+                products = []
+            else:
+                products = json.loads(content)
 
     return render_template("index.html", products=products)
-
-
-@app.route("/update-stock", methods=["POST"])
-def update_stock():
-
-    data = request.get_json()
-
-    with open(DATA_FILE, "w") as f:
-        json.dump(data, f)
-
-    return jsonify({"status": "success"})
-
